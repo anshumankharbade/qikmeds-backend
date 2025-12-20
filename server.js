2b88885fb97b64db0742b7050798486223fc683b
Anshuman Sohan Kharbade <anshumankharbade28@gmail.com>
2025-12-20 11:32:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Allow all origins to fix CORS
 app.use(cors({
-  origin: "*",
+  origin: "/",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
